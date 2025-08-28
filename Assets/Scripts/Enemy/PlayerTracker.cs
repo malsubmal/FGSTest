66320b7d2e4ac1bbf2686474e7e0b24a833bd2c0
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-28 16:44:31 +0700

    track player position

---
+++ Assets/Scripts/Enemy/PlayerTracker.cs
@@ -0,0 +1,34 @@
+﻿using FGSTest.Payload;
+using SuperMaxim.Messaging;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace FGSTest.Enemy
+{
+    public class PlayerTracker : MonoBehaviour
+    {
+        [SerializeField] private float _toleranceRange;
+        
+        public Vector3 PlayerPosition => _playerTransform.position;
+        public float PlayerRadius => _playerRadius;
+        
+        private float _playerRadius;
+        
+        private Transform _playerTransform;
+        private void Awake()
+        {
+            Messenger.Default.Subscribe<PlayerSpawnPayload>(OnPlayerSpawn);
+        }
+        
+        private void OnPlayerSpawn(PlayerSpawnPayload payload)
+        {
+            _playerTransform = payload.PlayerTransform;
+            _playerRadius = payload.PlayerRadius;
+        }
+
+        private void OnDestroy()
+        {
+            Messenger.Default.Unsubscribe<PlayerSpawnPayload>(OnPlayerSpawn);
+        }
+    }
+}

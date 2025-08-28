1c6963b2ce59dcc62203b811ba9db633ef5c7767
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-28 16:40:19 +0700

    add map utilities

---
+++ Assets/Scripts/Common/MeshBaker.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace FGSTest.Common
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    public class MeshBaker : MonoBehaviour
+    {
+        [SerializeField] private MeshFilter _combinedMeshFilter;
+        [SerializeField] private MeshCollider _meshCollider;
+
+        void Start()
+        {
+            if (_combinedMeshFilter == null)
+                _combinedMeshFilter = GetComponent<MeshFilter>();
+
+            // Collect source MeshFilters (children only)
+            var meshFilters = GetComponentsInChildren<MeshFilter>();
+            var instances = new List<CombineInstance>();
+
+            foreach (var mf in meshFilters)
+            {
+                if (mf == _combinedMeshFilter || mf.sharedMesh == null)
+                    continue;
+
+                instances.Add(new CombineInstance
+                {
+                    mesh = mf.sharedMesh,
+                    transform = _combinedMeshFilter.transform.worldToLocalMatrix * mf.transform.localToWorldMatrix
+                });
+            }
+
+            // Create the combined mesh
+            var combinedMesh = new Mesh
+            {
+                indexFormat = UnityEngine.Rendering.IndexFormat.UInt32 // supports >65k verts
+            };
+            combinedMesh.CombineMeshes(instances.ToArray(), true, true);
+
+            // Assign to MeshFilter
+            _combinedMeshFilter.sharedMesh = combinedMesh;
+
+            // Disable sources
+            foreach (var mf in meshFilters)
+            {
+                if (mf != _combinedMeshFilter)
+                    mf.gameObject.SetActive(false);
+            }
+
+            // Assign collider
+            if (_meshCollider)
+                _meshCollider.sharedMesh = combinedMesh;
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/MapConstraint.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace FGSTest.Environment
+{
+    public class MapConstraint : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _planeMesh;
+        [SerializeField] private float _border = 5;
+        
+        [SerializeField] private Vector3 _maxPos;
+        [SerializeField] private Vector3 _minPos;
+        
+        private void Awake()
+        {
+            BakeBound();
+        }
+
+        private void BakeBound()
+        {
+            var bounds = _planeMesh.bounds;
+            _maxPos = bounds.max;
+            _minPos = bounds.min;
+            _maxPos -= Vector3.one * _border;
+            _minPos += Vector3.one * _border;
+        }
+
+        public Vector3 SampleRandomPosInsideMesh()
+        {
+            Vector3 randomPos = new Vector3(
+                Random.Range(_minPos.x, _maxPos.x),
+                Random.Range(_minPos.y, _maxPos.y),
+                Random.Range(_minPos.z, _maxPos.z)
+            );
+            
+            return randomPos;
+        }
+
+        public Vector3 ClampXZPosToMap(Vector3 pos)
+        {
+            float x = Mathf.Clamp(pos.x, _minPos.x, _maxPos.x);
+            float y = Mathf.Clamp(pos.y, _minPos.y, _maxPos.y);
+            float z = Mathf.Clamp(pos.z, _minPos.z, _maxPos.z);
+
+            return new Vector3(x, y, z);
+        }
+        
+    }
+}

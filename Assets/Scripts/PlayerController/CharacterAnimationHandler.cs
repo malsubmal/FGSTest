cf736a4dd6195f67f2c105ded4d325e7a3b4e046
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-28 16:46:43 +0700

    animation + facing

---
+++ Assets/Scripts/PlayerController/CharacterAnimationHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Animancer;
+using UnityEngine;
+
+namespace FGSTest.PlayerController
+{
+    public class CharacterAnimationHandler : MonoBehaviour
+    {
+        [SerializeField] List<AnimationKeyValue> _animationList;
+        [SerializeField] AnimancerComponent _animancer;
+        
+        private Dictionary<AnimationKey, TransitionAsset> _clipTable;
+
+        private void Awake()
+        {
+            InitDictionary();
+        }
+
+        private void InitDictionary()
+        {
+            _clipTable = new Dictionary<AnimationKey, TransitionAsset>();
+            foreach (var keyValue in _animationList)
+            {
+                _clipTable.Add(keyValue.Key, keyValue.Clip);
+            }
+        }
+
+        public void PlayAnimation(AnimationKey key)
+        {
+            if (_clipTable is null)
+                InitDictionary();
+            
+            if (_clipTable is null)
+                return;
+            
+            if (_clipTable.TryGetValue(key, out var clip))
+            {
+                _animancer.Play(clip);
+            }
+        }
+    }
+    
+    [Serializable]
+    public struct AnimationKeyValue
+    {
+        public AnimationKey Key;
+        public TransitionAsset Clip;
+    }
+
+    [Serializable]
+    public enum AnimationKey
+    {
+        Idle = 0,
+        Jump = 1,
+        Landing = 2,
+        Walk = 3,
+        Run = 4,
+    }
+}
---
+++ Assets/Scripts/PlayerController/CharacterFacingDirection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace FGSTest.PlayerController
+{
+    public class CharacterFacingDirection : MonoBehaviour
+    {
+        [SerializeField] private Transform _visualTransform;
+        [SerializeField] private PlayerInputReceiver _playerInput;
+
+        private void Update()
+        {
+            if (_playerInput.PlayerInput.Movement == Vector2.zero)
+                return;
+            
+            var moveDir = _playerInput.PlayerInput.Movement;
+            var moveDirXZ = new Vector3(moveDir.x, 0, moveDir.y);
+            
+            _visualTransform.localEulerAngles = new Vector3(0, Mathf.Atan2(moveDirXZ.x, moveDirXZ.z) * Mathf.Rad2Deg, 0);
+        }
+    }
+}

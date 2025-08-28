d8abe6ad6043663c20c18ea1a6e546d3cdc934f5
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-28 16:39:25 +0700

    add key object

---
+++ Assets/Scripts/Environment/KeyObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FGSTest.Payload;
+using SuperMaxim.Messaging;
+using UnityEngine;
+
+namespace FGSTest.Environment
+{
+    public class KeyObject : MonoBehaviour
+    {
+        [SerializeField] private bool _isActive;
+        private Func<Collider, bool> _isColliderPlayer;
+
+        public void ActivateCollider()
+        {
+            _isActive = true;
+        }
+        
+        public void InitPlayerColliderChecker(Func<Collider, bool> checker)
+        {
+            _isColliderPlayer = checker;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_isActive) return;
+            
+            if (_isColliderPlayer == null) return;
+            
+            Debug.Log($"Collision Enter {other.name}");
+            
+            if (_isColliderPlayer(other))
+                ProcessCatchKey();
+        }
+        
+        private void ProcessCatchKey()
+        {
+            Messenger.Default.Publish(new CatchKeyPayload());
+            this.gameObject.SetActive(false);
+        }
+    }
+}

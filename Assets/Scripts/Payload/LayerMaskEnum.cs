7d83e7571f18370dfa91a065518783bd5b53811d
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-28 16:39:39 +0700

    add layermask enum

---
+++ Assets/Scripts/Payload/LayerMaskEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FGSTest.Payload
+{
+    [Flags]
+    public enum LayerMaskEnum
+    {
+        None = 0,
+        Default = 1 << 0,
+        Ground = 1 << 1,
+        Obstacle = 1 << 2,
+    }
+}

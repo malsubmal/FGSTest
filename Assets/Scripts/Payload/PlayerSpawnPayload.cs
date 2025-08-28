cc1183f45ce28d228de8c06873082a01a13cb9a7
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-28 16:41:23 +0700

    add noti player spawn

---
+++ Assets/Scripts/Payload/PlayerSpawnPayload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace FGSTest.Payload
+{
+    [Serializable]
+    public struct PlayerSpawnPayload
+    {
+        public int ColliderInstanceId;
+        public Transform PlayerTransform;
+        public float PlayerRadius;
+        
+        public PlayerSpawnPayload(Transform playerTransform, float playerRadius, int colliderInstanceId)
+        {
+            PlayerTransform = playerTransform;
+            PlayerRadius = playerRadius;
+            ColliderInstanceId = colliderInstanceId;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController/PlayerSpawnNotifier.cs
@@ -0,0 +1,16 @@
+﻿using FGSTest.Payload;
+using SuperMaxim.Messaging;
+using UnityEngine;
+
+namespace FGSTest.PlayerController
+{
+    public class PlayerSpawnNotifier : MonoBehaviour
+    {
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Transform _playerTransform;
+        private void Start()
+        {
+            Messenger.Default.Publish(new PlayerSpawnPayload(_playerTransform, _characterController.radius, _characterController.GetInstanceID()));
+        }
+    }
+}
